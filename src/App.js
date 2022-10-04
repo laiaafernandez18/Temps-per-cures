3fdf7e92119e4eca10e87b3cc638792a9eb2420f
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-04 10:41:25 +0200

    calendar setmore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-export default function App(props) {
-  const {
-    iframeSource = '<iframe src="https://book.timify.com?accountId=633bd418aa97e532ebc85e66&fullscreen=1&hideCloseButton=1&locale=es-es"></iframe>'
-  } = props;
-  return <div className="App" dangerouslySetInnerHTML={{__html: iframeSource}}></div>;
+import CalendarSetMore from "./Components/Calendar.SetMore";
+
+function App() {
+  return (
+    <>
+    <h1>Temps per Cures</h1>
+    <CalendarSetMore/>
+    </>
+  );
 }
+
+export default App;
---
+++ src/Components/Calendar.SetMore.jsx
@@ -0,0 +1,10 @@
+function CalendarSetMore() {
+    return (
+      <>
+      <script id="setmore_script" type="text/javascript" src="https://my.setmore.com/webapp/js/src/others/setmore_iframe.js"></script><a id="Setmore_button_iframe" style={{float:"none", position: "fixed", right: "-2px", top: "25%", display: "block", zindex: "20000"}} href="https://booking.setmore.com/scheduleappointment/d906fbe0-36bf-4999-b713-c73ef49088bf"><img border="none" src="https://storage.googleapis.com/full-assets/setmore/images/1.0/Calendar/Setmore-Book-Now.png" alt="Book an appointment with Temps-per-Cures using Setmore" /></a>
+      </>
+    );
+  }
+  
+  export default CalendarSetMore;
+  
