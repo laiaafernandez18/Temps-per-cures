c31c6fea52a9b122fd148d5987b882a67ea5e5e5
mflamanna <lamanna.mflorencia@gmail.com>
2022-10-04 09:17:14 +0200

    calendario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-function App() {
-  return (
-    <>
-    <p>Hola</p>
-    </>
-  );
+export default function App(props) {
+  const {
+    iframeSource = '<iframe src="https://book.timify.com?accountId=633bd418aa97e532ebc85e66&fullscreen=1&hideCloseButton=1&locale=es-es"></iframe>'
+  } = props;
+  return <div className="App" dangerouslySetInnerHTML={{__html: iframeSource}}></div>;
 }
-
-export default App;
